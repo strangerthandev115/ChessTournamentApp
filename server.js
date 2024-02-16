498a1bce5d9e2833337d9a868f9c3afade7df0e9
Bo Truelove <botruelove2002@gmail.com>
2024-02-16 10:06:49 -0600

    added node_modules, to be used in the backend. Also changed the structure of our webapp, so that it is a server that serves up the webpages instead of just a static webpage. This is so that we can use the server to handle the requests and responses for the data. We now have a working backend

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+// Route for inputPlayers.html
+app.get('/inputPlayers', (req, res) => {
+  res.sendFile(path.join(__dirname, 'inputPlayers', 'inputPlayers.html'));
+});
+
+// Route for prizeMoney.html
+app.get('/prizeMoney', (req, res) => {
+  res.sendFile(path.join(__dirname, 'prizeMoney', 'prizeMoney.html'));
+});
+
+// Route for scoreReport.html
+app.get('/scoreReport', (req, res) => {
+  res.sendFile(path.join(__dirname, 'scoreReport', 'scoreReport.html'));
+});
+
+// Route for tournament.html
+app.get('/tournament', (req, res) => {
+  res.sendFile(path.join(__dirname, 'tournament', 'tournament.html'));
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
