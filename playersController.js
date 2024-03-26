0f94b306d3d51567249d7259a0c024de46fefc21
Timothy Morren <timmoren01@gmail.com>
2024-03-26 10:02:25 -0500

    made changes

--- playersController.js
+++ playersController.js
@@ -1,10 +1,11 @@
-let players = ["John", "Sally", "Joe", "Tim", "Gatlin", "Jordan", "Tommy", "Emily"];
+let players = [];
 let samplePlayers = [
     {
         id: 10000001,
         fname: "John",
         lname: "Doe",
-        rating: 1800,
+        rapidRating: 1700,
+        quickRating: 1800,
         section: '',
         matchInfo: ['10000010','10000009','10000008','10000007','10000006','10000005','10000004','10000003','10000002'],
         scoreReport: [0,0,0,0,0,0,0,0,0]
@@ -13,7 +14,8 @@ let samplePlayers = [
         id: 10000002,
         fname: "Jane",
         lname: "Smith",
-        rating: 2000,
+        rapidRating: 1500,
+        quickRating: 1200,
         section: '1',
         matchInfo: ['10000009','10000007','10000005','10000003','10000010','10000008','10000006','10000004','10000001'],
         scoreReport: [0,0,0,0,0,0,0,0,0]
@@ -22,7 +24,8 @@ let samplePlayers = [
         id: 10000003,
         fname: "Mike",
         lname: "Johnson",
-        rating: 1700,
+        rapidRating: 800,
+        quickRating: 500,
         section: '2',
         matchInfo: ['10000008','10000006','10000004','10000002','10000009','10000007','10000005','10000001','10000010'],
         scoreReport: [0,0,0,0,0,0,0,0,0]
@@ -31,7 +34,8 @@ let samplePlayers = [
         id: 10000004,
         fname: "Sarah",
         lname: "Williams",
-        rating: 1900,
+        rapidRating: 600,
+        quickRating: 500,
         section: '3',
         matchInfo: ['10000007','10000005','10000003','10000010','10000008','10000006','10000001','10000002','10000009'],
         scoreReport: [0,0,0,0,0,0,0,0,0]
@@ -40,7 +44,8 @@ let samplePlayers = [
         id: 10000005,
         fname: "David",
         lname: "Brown",
-        rating: 1600,
+        rapidRating: 500,
+        quickRating: 800,
         section: '3',
         matchInfo: ['10000006','10000004','10000002','10000009','10000007','10000001','10000003','10000010','10000008'],
         scoreReport: [0,0,0,0,0,0,0,0,0]
@@ -49,7 +54,8 @@ let samplePlayers = [
         id: 10000006,
         fname: "Emily",
         lname: "Taylor",
-        rating: 2100,
+        rapidRating: 500,
+        quickRating: 1200,
         section: '2',
         matchInfo: ['10000005','10000003','10000010','10000008','10000001','10000004','10000002','10000009','10000007'],
         scoreReport: [0,0,0,0,0,0,0,0,0]
@@ -58,7 +64,8 @@ let samplePlayers = [
         id: 10000007,
         fname: "Michael",
         lname: "Anderson",
-        rating: 1750,
+        rapidRating: 100,
+        quickRating: 200,
         section: '',
         matchInfo: ['10000004','10000002','10000009','10000001','10000005','10000003','10000010','10000008','10000006'],
         scoreReport: [0,0,0,0,0,0,0,0,0]
@@ -67,7 +74,8 @@ let samplePlayers = [
         id: 10000008,
         fname: "Jessica",
         lname: "Martinez",
-        rating: 1850,
+        rapidRating: 9999,
+        quickRating: 9999,
         section: '',
         matchInfo: ['10000003','10000010','10000001','10000006','10000004','10000002','10000009','10000007','10000005'],
         scoreReport: [0,0,0,0,0,0,0,0,0]
@@ -76,7 +84,8 @@ let samplePlayers = [
         id: 10000009,
         fname: "Daniel",
         lname: "Thomas",
-        rating: 9999,
+        rapidRating: 9999,
+        quickRating: 800,
         section: '',
         matchInfo: ['10000002','10000001','10000007','10000005','10000003','10000010','10000008','10000006','10000004'],
         scoreReport: [0,0,0,0,0,0,0,0,0]
@@ -85,7 +94,8 @@ let samplePlayers = [
         id: 10000010,
         fname: "Olivia",
         lname: "Garcia",
-        rating: 2050,
+        rapidRating: 9999,
+        quickRating: 1800,
         section: '',
         matchInfo: ['10000001','10000008','10000006','10000004','10000002','10000009','10000007','10000005','10000003'],
         scoreReport: []
