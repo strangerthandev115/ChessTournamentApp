5a5711243b627dcfd0ea4b0c21d4383d69789553
strangerthandev115 <139648342+strangerthandev115@users.noreply.github.com>
2024-04-15 21:11:57 -0500

    Updated writeTD
    Updated writeTD as well as the dbfGenerator html such that an event ID must be provided and then the results of the tournament will be (mostly) generated

--- dbfGenerator/dbfGenerator.js
+++ dbfGenerator/dbfGenerator.js
@@ -4,12 +4,22 @@ const button = document.querySelector('#generateDBFs');
 // Add a click event listener to the button
 button.addEventListener('click', async () => {
     try {
+        // Get the event ID from the input element
+        const eventID = document.getElementById('eventid').value;
+
+        // Validate the event ID
+        if (eventID.length !== 9) {
+            alert('Event ID must be exactly 9 characters long.');
+            return; // Exit the function if event ID length is not 9
+        }
+        
         // Send a fetch request to dbfController.js
         const response = await fetch('/dbfGenerator', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
+            body: JSON.stringify({ eventID })
         });
 
         // Check if the request was successful
